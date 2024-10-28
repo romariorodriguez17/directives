440ce6a8eea74822e2934d9e2a5b03a959908956
Romario Rodriguez <romariorodriguez17@gmail.com>
2024-10-28 14:47:03 -0500

    se realiza el ejercicio del ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Student {
+  nombre: string;
+  estado: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'directives';
+  //ngFor
+  listStudents: Student[] = [
+    { nombre: 'Romario Rodriguez', estado: 'promocionado' },
+    { nombre: 'Luis Perez', estado: 'regular' },
+    { nombre: 'ronaldo nazario', estado: 'retirado' },
+  ]
 }
