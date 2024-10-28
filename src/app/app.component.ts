1b78e1bc8433a2e21c7325e57a1fd721e9fab914
Romario Rodriguez <romariorodriguez17@gmail.com>
2024-10-28 15:13:07 -0500

    se añade la directiva ng-If

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,23 @@ interface Student {
 })
 export class AppComponent {
   title = 'directives';
+  mostrar = true
   //ngFor
   listStudents: Student[] = [
     { nombre: 'Romario Rodriguez', estado: 'promocionado' },
     { nombre: 'Luis Perez', estado: 'regular' },
     { nombre: 'ronaldo nazario', estado: 'retirado' },
+    { nombre: 'Ana Gomez', estado: 'promocionado' },
+    { nombre: 'Carlos Sanchez', estado: 'regular' },
+    { nombre: 'Maria Lopez', estado: 'retirado' }
   ]
+
+  //NgIf
+
+  // toogle(): void {
+  //   this.mostrar = !this.mostrar
+  // }
+  toogle(): void {
+    this.listStudents.length && this.listStudents.pop();
+  }
 }
